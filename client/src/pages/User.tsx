19aa7057bdfe510323d5aed7c759f78b95520e00
Gleb BUSHUKIN <gleb.bushukin@gmail.com>
2025-02-03 14:02:39 +0100

    init react native app

--- client/src/pages/User.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router';
-import { Title } from '../shared/text';
-
-export default function User() {
-  const { name } = useParams();
-
-  return <Title>User {name || ''}</Title>;
-}
